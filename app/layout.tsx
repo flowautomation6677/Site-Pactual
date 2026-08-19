d6d9d4a92ae827cd0b31293983cc84b424be829b
Luiz <flowautomation6677@gmail.com>
2026-08-18 22:07:40 -0300

    feat: restore R3F shield

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,7 +62,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     >
       <body className="min-h-full flex flex-col font-sans bg-white text-pactual-graphite">
         {children}
-        <Script src="https://ajax.googleapis.com/ajax/libs/model-viewer/4.0.0/model-viewer.min.js" type="module" strategy="lazyOnload" />
       </body>
     </html>
   );
