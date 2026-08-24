95b4dd25bc889eff1ee236a821805a17c4bee101
Luiz <flowautomation6677@gmail.com>
2026-08-24 18:46:14 -0300

    Alteração página4 e teste

--- app/jornada5/page.tsx
+++ app/jornada5/page.tsx
@@ -1,9 +1,9 @@
-﻿import ScrollStoryFrames from "@/components/sections/ScrollStoryFrames";
-import CorporateGlassHeader from "@/components/header/CorporateGlassHeader";
+﻿import CorporateGlassHeader from "@/components/header/CorporateGlassHeader";
+import HeroSection from "@/components/hero/HeroSection";
 import FloatingWhatsApp from "@/components/FloatingWhatsApp";
 import dynamic from "next/dynamic";
 
-// Abaixo da dobra: Lazy load forçado para não bloquear o FCP/LCP
+// Abaixo da dobra: Lazy load forÃ§ado para nÃ£o bloquear o FCP/LCP
 const VehicleSelector = dynamic(() => import("@/components/sections/VehicleSelector"), { ssr: true });
 const BenefitsBar = dynamic(() => import("@/components/sections/BenefitsBar"), { ssr: true });
 const ComparisonTable = dynamic(() => import("@/components/sections/ComparisonTable"), { ssr: true });
@@ -16,53 +16,39 @@ const PartnersSection = dynamic(() => import("@/components/sections/PartnersSect
 const InstitutionalFooter = dynamic(() => import("@/components/footer/InstitutionalFooter"), { ssr: true });
 const StickyMobileConversionFooter = dynamic(() => import("@/components/footer/StickyMobileConversionFooter"));
 
-export default function Jornada4Page() {
+export default function Home() {
   return (
-    <main className="min-h-screen font-sans selection:bg-pactual-blue/30 selection:text-pactual-graphite">
+    <main className="min-h-screen bg-white font-sans selection:bg-pactual-blue/30 selection:text-pactual-graphite">
       <FloatingWhatsApp />
       <CorporateGlassHeader />
       
       <div id="cota-agora">
-        <ScrollStoryFrames 
-          frameCount={70}
-          startIndex={1}
-          imagePrefix="/teste/frames_7fps/frame_"
-          mobileImagePrefix="/teste/frames_7fps_9x16/frame_"
-          mobileBreakpoint={768}
-          imageExtension=".webp"
-          padLength={3}
-        />
+        <HeroSection />
       </div>
       
-      {/* Container com fundo branco para sobrepor o canvas fixo ao rolar para o resto do site */}
-      <div className="relative z-20 bg-white">
-        <RiskSection />
-        
-        <VehicleSelector />
-        <div id="coberturas">
-          <BenefitsBar />
-        </div>
-        
-        <PricingPlans />
-        
-        <ComparisonTable />
-        <div id="como-funciona">
-          <HowItWorks />
-        </div>
-        <div id="depoimentos">
-          <SocialProof />
-        </div>
-        <div id="faq">
-          <FaqAccordion />
-        </div>
-        
-        <PartnersSection />
-        <InstitutionalFooter />
+      <RiskSection />
+      
+      <VehicleSelector />
+      <div id="coberturas">
+        <BenefitsBar />
       </div>
       
-      <div className="relative z-30">
-        <StickyMobileConversionFooter />
+      <PricingPlans />
+      
+      <ComparisonTable />
+      <div id="como-funciona">
+        <HowItWorks />
       </div>
+      <div id="depoimentos">
+        <SocialProof />
+      </div>
+      <div id="faq">
+        <FaqAccordion />
+      </div>
+      
+      <PartnersSection />
+      <InstitutionalFooter />
+      <StickyMobileConversionFooter />
     </main>
   );
 }
